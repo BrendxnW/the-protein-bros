a600510b41d6704f341f2569dce3c16a0780f230
Tesneem El-kheir <165869766+telkheir@users.noreply.github.com>
2026-07-30 16:43:03 -0500

    Products queries

--- database/dml.sql
+++ database/dml.sql
@@ -4,7 +4,7 @@
 -- Products Queries --
 ----------------------
 
--- get all Products and their Brand, ProteinType, Flavor, and cost for the browse Products page
+-- Get all Products data to display in the Products page
 select Products.productID as "Item ID", Products.productName as "Product", Brands.brandName as "Brand",
 Flavors.flavorName as "Flavor", ProteinTypes.proteinType as "Protein", Products.cost as "Price"
 from Products
@@ -12,11 +12,54 @@ from Products
     join ProteinTypes on ProteinTypes.proteinTypeID = Products.proteinTypeID
     join Flavors on Flavors.flavorID = Products.flavorID;
 
+-- Add a new product
+insert into Products (productName, cost, brandID, proteinTypeID, flavorID, stockQuantity)
+values
+    (:productNameInput, :costInput, :brandIDInput, :proteinTypeIDInput, :flavorIDInput, :stockQuantityInput)
+
+-- get Brand ID and Name to populate Brands dropdown
+select brandID, brandName
+from Brands;
+
+-- get Protein Type ID and Name to populate Protein Type dropdown
+select proteinTypeID, proteinType
+from ProteinTypes;
+
+-- get Flavor ID and Name to populate Flavors dropdown
+select flavorID, flavorName
+from Flavors;
+
+-- get a Product's data for the Update form
+select Products.productID, Products.productName, Products.cost, Products.stockQuantity,
+Brands.brandName, ProteinTypes.proteinType, Flavors.flavorName
+from Products
+    join Brands on Brands.brandID = Products.brandID
+    join ProteinTypes on ProteinTypes.proteinTypeID = Products.proteinTypeID
+    join Flavors on Flavors.flavorID = Products.flavorID
+where Products.productID = :selected_product_ID
+
+-- update a Product's data given its ID
+update Products
+    set productName = :productNameInput, cost = :costInput, stockQuantity = :stockQuantityInput,
+    brandID = :brandIDInput, proteinTypeID = :proteinTypeIDInput, flavorID = :flavorIDInput
+    where productID = :selectedProductID;
+
+-- delete a Product's data given its ID
+delete from Products
+where productID = :selectedProductID;
+
+
+
 
 ------
 -- Customers Queries
 ------
 
+-- Populate table in Customers page to display all customers and their data
+select customerID as "Customer ID", customerName as "Customer", phoneNumber as "Phone Number",
+address as "Address"
+from Customers
+order by customerID asc;
 
 
 ------
