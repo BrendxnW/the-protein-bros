26d51c3a5d181c149290ae3b3c0297a88d5518b2
ailstrup <ilstrupa@oregonstate.edu>
2026-07-30 19:41:23 -0400

    protein types in table format

--- frontend/src/pages/ProteinTypes.jsx
+++ frontend/src/pages/ProteinTypes.jsx
@@ -1,8 +1,32 @@
 
 function ProteinTypes({backendURL}) {
+    const proteinTypesList = [
+        {id: 1, type: "Whey"},
+        {id: 2, type: "Casein"},
+        {id: 3, type: "Pea"},
+        {id: 4, type: "Soy"},
+        {id: 5, type: "Egg"}
+    ];
+    
     return (
         <>
-        <h1>ProteinTypes Page</h1>
+        <h1>Protein Types Page</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Protein Type ID</th>
+                    <th>Protein Type</th>
+                </tr>
+            </thead>
+            <tbody>
+                {proteinTypesList.map((i) => (
+                    <tr key={i.id}>
+                        <td>{i.id}</td>
+                        <td>{i.type}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
         </>
     )
 }
