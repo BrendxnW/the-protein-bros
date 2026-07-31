c8d1942021775cfaa2510ee0f9074e753541525c
BrendxnW <BrendonHorse18@gmail.com>
2026-07-30 19:58:50 -0700

    changed navigation links to work with deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import Products from './pages/Products.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
-<<<<<<< HEAD
-  <StrictMode>
-    <Products />
-  </StrictMode>,
-)
-=======
-=======
->>>>>>> a45a4994e6f73e30f2cbc9486edcd9a8b6a92dd3
+createRoot(document.getElementById("root")).render(
     <StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </StrictMode>,
+    </StrictMode>
 );
-<<<<<<< HEAD
->>>>>>> a01524f6cad6642f93a926843032b70d9dce163a
-=======
->>>>>>> a45a4994e6f73e30f2cbc9486edcd9a8b6a92dd3
